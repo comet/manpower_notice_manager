caa7ebf17b68b021bbd7f758128cadadb2d7fd36
Slowcoach <knkarithi@gmail.com>
2013-02-05 06:40:18 +0300

    Added general arguments

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -2,7 +2,7 @@ class Notice < ActiveRecord::Base
   attr_accessible :company, :edition, :edition_date, :id_number, :name, :no_of_appearance, :page_no
 #  validates :name, :company, :edition, :no_of_appearance,:page_no,:presence => true
  # validates :id_number,:page_no,:no_of_appearance, :numericality => true
-  def self.general_search(value)
+  def self.general_search(value,perpage)
     conditions = []
     value = value<<"%"
     prep = "%"
@@ -13,9 +13,9 @@ class Notice < ActiveRecord::Base
       conditions[0]+=" OR company LIKE ?"
       conditions<< value
 
-      where(conditions)
+      Notice.paginate(:page => perpage,:per_page => 15).where(conditions)
     else
-      Notice.all
+      Notice.paginate(:page => perpage, :per_page => 15)
     end
     
   end
